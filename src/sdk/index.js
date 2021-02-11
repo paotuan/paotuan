452258762cc68e639cb1cbddeaf977948375773d
maliut <lqn619@163.com>
2021-02-11 16:59:45 +0800

    remove devlog

--- src/sdk/index.js
+++ src/sdk/index.js
@@ -3,3 +3,20 @@ export const createTIM = window.TIMC // for bot
 export default createTIM() // as original TIM, for user
 
 delete window.TIMC
+
+window.genTestUserSig = (userID, SDKAPPID, SECRETKEY) => {
+  /**
+   * 签名过期时间，建议不要设置的过短
+   * <p>
+   * 时间单位：秒
+   * 默认时间：7 x 24 x 60 x 60 = 604800 = 7 天
+   */
+  const EXPIRETIME = 604800
+
+  const generator = new window.LibGenerateTestUserSig(SDKAPPID, SECRETKEY, EXPIRETIME)
+  const userSig = generator.genTestUserSig(userID)
+  return {
+    SDKAppID: SDKAPPID,
+    userSig: userSig
+  }
+}
